89ea082d84e8d5a9d86b2608676dbee4f8ab4c23
Nicolas Fumo <49803280+NicolasFumo@users.noreply.github.com>
2023-11-04 19:05:35 -0300

    Create program.cs

---
+++ TP2/V1/program.cs
@@ -0,0 +1,9 @@
+// Se desea crear una agenda telefonica para guardar
+// el nombre, apellido y telefono de un grupo de personas.
+// El sistema tiene que permitir agregar personas, quitarlas de la agenda
+// y buscar por nombre o mostrar la totalidad de contactos
+// como segunda parte, permitir editar un contacto
+
+using AgendaTelefonica;
+
+List<Contacto> agenda = new List<Contacto>();
